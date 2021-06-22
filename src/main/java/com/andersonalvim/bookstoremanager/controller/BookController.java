49690f5635ffe5c9a75db6e0410ac5f9495dbc06
Anderson Alvim <alvimsouza@gmail.com>
2021-06-22 14:45:20 -0300

    Refactores functionality for book creation

---
+++ src/main/java/com/andersonalvim/bookstoremanager/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.andersonalvim.bookstoremanager.controller;
+
+import com.andersonalvim.bookstoremanager.dto.MessageResponseDTO;
+import com.andersonalvim.bookstoremanager.entity.Book;
+import com.andersonalvim.bookstoremanager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody Book book) {
+        return bookService.create(book);
+    }
+}
